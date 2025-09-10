9e1aec7ae7886ea3fd060971735755872306782b
mariamora31 <142507796+mariamora31@users.noreply.github.com>
2025-09-10 20:57:46 -0300

    OK

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaperguntas=querySelector(".caixa-perguntas");
+const caixaAlternativa=document.querySelector(".caixa- alternativas");
+const caixaResultado=document;querySelector(".caixa-resulatdo");
+const textoResultado=document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+enunciado:"assim que saiu do colégio você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios. Qual é o seu primeiro pensamento?",
+alternativas:[
+    {
+        texto: "isso é assustador!",
+        afirmacao:"No início, eu ficaria com medo do que essa tecnologia pode fazer."
+
+    },
+    {
+        
+    }
+]
+
+}
+]
